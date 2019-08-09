75003743029239df5c4ba9c7f2f81c023da52dbb
K. Sun <akaronsun@gmail.com>
2019-08-09 11:44:28 +0800

    update pubsubNode.js to latest version in master branch

--- pubsubNode.js
+++ pubsubNode.js
@@ -11,50 +11,54 @@ const os = require('os');
 const path = require('path');
 
 // Common Bucket Tx
-const fields = 
+const mfields =
 [
-	{name: 'nonce', length: 32, allowLess: true, default: new Buffer([]) },
-	{name: 'originAddress', length: 20, allowZero: true, default: new Buffer([]) },
-	{name: 'ipfsHash', length: 32, allowLess: true, default: new Buffer([]) },
-	{name: 'since', length: 32, allowLess: true, default: new Buffer([]) },
-	{name: 'reply', length: 32, allowLess: true, default: new Buffer([]) },
-	{name: 'comment', length: 32, allowLess: true, default: new Buffer([]) },
-	{name: 'score', length: 32, allowZero: true, default: new Buffer([]) }, // review only
-	{name: 'category', length: 32, allowLess: true, default: new Buffer([]) }, // new fields to be handled by ??? 
-   	{name: 'v', allowZero: true, default: new Buffer([0x1c]) },
-   	{name: 'r', allowZero: true, length: 32, default: new Buffer([]) },
-   	{name: 's', allowZero: true, length: 32, default: new Buffer([]) }
+        {name: 'opround', length: 32,   allowLess: true, default: Buffer.from([]) },  // opround integer
+        {name: 'account', length: 20,   allowZero: true, default: Buffer.from([]) },  // user (autherized) address
+        {name: 'comment', length: 32,   allowLess: true, default: Buffer.from([]) },  // ipfs hash (comment)
+        {name:   'title', length: 1024, allowLess: true, allowZero: true, default: Buffer.from([]) },  // article title
+        {name:     'url', length: 1024, allowLess: true, allowZero: true, default: Buffer.from([]) },  // article url
+        {name:     'aid', length: 32,   allowZero: true, default: Buffer.from([]) },  // sha256(title+domain), bytes32
+        {name:     'oid', length: 32,   allowLess: true, default: Buffer.from([]) },  // participating game round ID, bytes32
+        {name: 'v1block', length: 32,   allowLess: true, default: Buffer.from([]) },  // 1st vote block
+        {name:  'v1leaf', length: 32,   allowLess: true, default: Buffer.from([]) },  // 1st vote txhash
+        {name: 'v2block', length: 32,   allowLess: true, default: Buffer.from([]) },  // 2nd vote (claim) block
+        {name:  'v2leaf', length: 32,   allowLess: true, default: Buffer.from([]) },  // 2nd vote (claim) txhash
+        {name:   'since', length: 32,   allowLess: true, default: Buffer.from([]) },  // timestamp, uint
+        {name: 'v1proof', length: 768,  allowLess: true, allowZero: true, default: Buffer.from([]) },  // 1st vote merkle proof
+        {name:  'v1side', length: 3,    allowLess: true, allowZero: true, default: Buffer.from([]) },  // 1st vote merkle proof (side)
+        {name: 'v2proof', length: 768,  allowLess: true, allowZero: true, default: Buffer.from([]) },  // 2nd vote merkle proof
+        {name:  'v2side', length: 3,    allowLess: true, allowZero: true, default: Buffer.from([]) },  // 2nd vote merkle proof (side)
+        {name:       'v', allowZero: true, default: Buffer.from([0x1c]) },
+        {name:       'r', allowZero: true, length: 32, default: Buffer.from([]) },
+        {name:       's', allowZero: true, length: 32, default: Buffer.from([]) }
 ];
 
-// Validator Only
-const active =
+const pfields =
 [
-	{name: 'validator', length: 20, allowZero: true, default: new Buffer([]) },
-	{name: 'originAddress', length: 20, allowZero: true, default: new Buffer([]) },
-	{name: 'ipfsHash', length: 32, allowLess: true, default: new Buffer([]) },
-	{name: 'since', length: 32, allowLess: true, default: new Buffer([]) },
-	{name: 'agree', length: 32, allowLess: true, default: new Buffer([]) },
-	{name: 'disagree', length: 32, allowLess: true, default: new Buffer([]) },
-	{name: 'score', length: 32, allowZero: true, default: new Buffer([]) }, // review only
-	{name: 'category', length: 32, allowLess: true, default: new Buffer([]) }, // new fields to be handled by ??? 
-   	{name: 'v', allowZero: true, default: new Buffer([0x1c]) },
-   	{name: 'r', allowZero: true, length: 32, default: new Buffer([]) },
-   	{name: 's', allowZero: true, length: 32, default: new Buffer([]) }
+        {name: 'nonce', length: 32, allowLess: true, default: Buffer.from([]) },
+        {name: 'pending', length: 32, allowLess: true, default: Buffer.from([]) },
+        {name: 'validator', length: 20, allowZero: true, default: Buffer.from([]) },
+        {name: 'cache', length: 32, allowLess: true, default: Buffer.from([]) }, // ipfs hash of [txhs, txpd, txdt]
+        {name: 'since', length: 32, allowLess: true, default: Buffer.from([]) },
+        {name: 'v', allowZero: true, default: Buffer.from([0x1c]) },
+        {name: 'r', allowZero: true, length: 32, default: Buffer.from([]) },
+        {name: 's', allowZero: true, length: 32, default: Buffer.from([]) }
 ];
 
-const summary = 
-[
-	{name: 'validator', length: 20, allowZero: true, default: new Buffer([]) },
-	{name: 'originAddress', length: 20, allowZero: true, default: new Buffer([]) },
-	{name: 'start', length: 32, allowLess: true, default: new Buffer([]) },
-	{name: 'end', length: 32, allowLess: true, default: new Buffer([]) },
-	{name: 'spend', length: 32, allowLess: true, default: new Buffer([]) },
-	{name: 'gain', length: 32, allowLess: true, default: new Buffer([]) },
-	{name: 'accuRewards', length: 32, allowLess: true, default: new Buffer([]) },
-   	{name: 'v', allowZero: true, default: new Buffer([0x1c]) },
-   	{name: 'r', allowZero: true, length: 32, default: new Buffer([]) },
-   	{name: 's', allowZero: true, length: 32, default: new Buffer([]) }
-];
+const keyCheck = (obj) => (k) =>
+{
+	if (!k in obj) return false;
+	if (typeof(obj[k]) === 'undefined') return false;
+	if (obj[k] === null) return null;
+	return true;
+}
+
+const keyCheckNoNull = (obj) => (k) =>
+{
+	let rc = keyCheck(obj)(k);
+	return rc === null ? false : rc;
+}
 
 class PubSub extends EventEmitter 
 {
@@ -64,28 +68,30 @@ class PubSub extends EventEmitter
 		let opts = { gossip: {}, ...options };
 		this.port  = opts.port || 0;
   		this.swarm = swarm(opts);
+		this.topicList = [];
 		this.firstConn = false;
-	        this.initialized = false;
+		this.initialized = false;
+		this.store;
 
 		this.join = (topic) =>
 		{
   			if (!topic || typeof topic !== 'string') { throw new Error('topic must be set as a string') }
-			if (typeof this.topic !== 'undefined') this.leave();
-			this.topic = topic;
 			this.seen  = { init: Math.floor(Date.now()/1000), logs: {}, seen: {} };
-  			return this.swarm.join(this.topic);
+			this.topicList.push(topic);
+  			return this.swarm.join(topic);
 		}
 
-		this.leave = () =>
+		this.leave = (topic) =>
 		{
-			if (typeof this.topic === 'undefined') return true;
-			return this.swarm.leave(this.topic);
+			if (this.topicList.indexOf(topic) === -1) return true;
+			this.topicList.splice(this.topicList.indexOf(topic), 1);
+			return this.swarm.leave(topic);
 		}
 
 		this.stats = () =>
 		{
 			return {
-				topics: this.topic,
+				topics: this.topicList,
 				peerseen: this.swarm._peersSeen,
 				connecting: this.swarm.connecting,
 				upcomming: this.swarm.queued,
@@ -104,43 +110,42 @@ class PubSub extends EventEmitter
 				fs.writeFileSync(path.join(os.homedir(), '.optract_keys'), JSON.stringify(this.gossip.keys))
 			}
 
-                        this.gossip.__data_filter = (msgData) =>
-                        {
-                                let msg = msgData.data;
+			// overload gossip.__data_filter for Optract
+			this.gossip.__data_filter = (msgData) =>
+			{
+				let msg = msgData.data;
 
-                                // - msg requires to contain "topic"
-                                if (typeof(msg.topic) === 'undefined') return false;
-                                // - topic needs to be in this.topicList
-                                if (this.topicList.length === 0 || this.topicList.indexOf(msg.topic) === -1) return false;
+                        	// - msg requires to contain "topic"
+                        	if (typeof(msg.topic) === 'undefined') return false;
+                        	// - topic needs to be in this.topicList
+                        	if (this.topicList.length === 0 || this.topicList.indexOf(msg.topic) === -1) return false;
 
-                                // - check encoded RLPx by topic match
-                                if (msg.topic === 'Optract') {
-                                        try {
-                                                let rlpx = Buffer.from(msg.msg);
-                                                let rlp = this.handleRLPx(mfields)(rlpx); // proper format;
+                        	// - check encoded RLPx by topic match
+                        	if (msg.topic === 'Optract') {
+                                	try {
+                                        	let rlpx = Buffer.from(msg.msg);
+	                                        let rlp = this.handleRLPx(mfields)(rlpx); // proper format;
 
-                                                if (rlp !== null) {
-                                                        return true;
-                                                } else {
-                                                        rlp = this.handleRLPx(pfields)(rlpx); // proper format;
-                                                        if (rlp !== null) {
-                                                                return true;
-                                                        }
-                                                }
-                                        } catch (err) {
-                                                return false;
-                                        }
-                                }
-                        }
+        	                                if (rlp !== null) {
+                        	                        return true;
+                                	        } else {
+      	                                                rlp = this.handleRLPx(pfields)(rlpx); // proper format;
+        	                                        if (rlp !== null) {
+                                                        	return true;
+                                                	}
+                                        	}
+                                	} catch (err) {
+						return false;
+                                	}
+                        	}	
+			}
 
   			this.id = this.gossip.keys.public; // should eventually use ETH address
 			console.log('My ID: ' + this.id);
 
-		  	this.gossip.on('message', (msg) => {
+		  	this.gossip.on('message', (msg, info) => {
 				//console.log('get Message'); console.dir(msg);
-				if (this.filterSeen(msg) && this.throttlePeer(msg.data)) {
-					this.emit('incomming', msg);
-				}
+				this.filterSeen(msg) && this.throttlePeer(info) && this.validateMsg(msg); 
   			})
 
 			// default dummy incomming handler
@@ -148,7 +153,6 @@ class PubSub extends EventEmitter
 				console.log('message passed filters, incomming event emitted...');
 			});
 
-			this.firstConn = false;
   			this.swarm.on('connection', (connection) => 
 			{
     				console.log("\nFound " + this.swarm.connected + ' connected ' + (this.swarm.connected === 1 ? 'peer' : 'peers') );
@@ -161,7 +165,7 @@ class PubSub extends EventEmitter
     				}
   			});
 
-	                this.initialized = false;
+			this.initialized = true;
 		}
 
 		// encode if packet is object, decode if it is RLPx
@@ -172,14 +176,14 @@ class PubSub extends EventEmitter
                                 ethUtils.defineProperties(m, fields, packet);
                                 return m;
                         } catch(err) {
-                                console.trace(err);
-                                return {};
+                                // console.trace(err);
+                                return null;
                         }
                 }
 
-		this.filterSeen = (msg) =>
+		this.filterSeen = (msgData) =>
 		{
-			msg = JSON.stringify(msg);
+			let msg = JSON.stringify(msgData);
 			let timeNow = Math.floor(Date.now()/1000);
 			let hashID = ethUtils.bufferToHex(ethUtils.sha256(Buffer.from(msg)));
 			if (typeof(this.seen.logs[hashID]) !== 'undefined' && timeNow - this.seen.logs[hashID] < 10000 ) {
@@ -209,23 +213,49 @@ class PubSub extends EventEmitter
 			}
 		}
 
-		this.validateMsg = (msg) =>
+		this.ping = (ipfsHash) => { return true }; // placeholder
+
+		this.validateMsg = (msgData) =>
 		{
-			// for now, only validate RLPx format, will also validate RLPx payload signature and confirm active membership via smart contract.
-			/*
-			try {
-				this.handleRLPx(fields)(msg);
-				return true;
-			} catch (err) {
-				return false;
+			let msg = msgData.data;
+
+			// - msg requires to contain "topic"
+			if (typeof(msg.topic) === 'undefined') return false;
+			// - topic needs to be in this.topicList
+			if (this.topicList.length === 0 || this.topicList.indexOf(msg.topic) === -1) return false;
+
+			// - check encoded RLPx by topic match
+			if (msg.topic === 'Optract') {
+				try {
+					let rlpx = Buffer.from(msg.msg);
+					let rlp = this.handleRLPx(mfields)(rlpx); // proper format;
+
+					if (rlp !== null) {
+						console.log(`DEBUG: incomming tx...`);
+						if (typeof(msg.store) !== 'undefined') {
+							this.ping(msg.store);
+						}
+
+						return this.emit('incomming', {topic: msg.topic, data: rlp});
+					} else {
+						console.log(`DEBUG: syncing pool...`)
+						rlp = this.handleRLPx(pfields)(rlpx); // proper format;
+						if (rlp !== null) {
+							return this.emit('onpending', {topic: msg.topic, data: rlp});
+						}
+					}
+				} catch (err) {
+					console.trace(err);
+					// more (pubsub / gossip) peer management can be utilized to deal with bad behaviors...
+				}
 			}
-			*/
-			return true; // temp fix for testing
 		}
 
-		this.publish = (msg) =>
+		this.publish = (topic, msg) =>
 		{
-			if (typeof(msg) !== 'object') msg = { data: {msg, public: this.id} }; // secure-gossip requires the key named "data" ...
+			if (this.topicList.length === 0 || this.topicList.indexOf(topic) === -1) return false; 
+			msg = { data: {topic, msg} }; // secure-gossip requires the key named "data" ...
+			if (typeof(this.store) !== 'undefined') msg.data['store'] = this.store;
     			return this.gossip.publish(msg)
 		}
 
@@ -237,8 +267,15 @@ class PubSub extends EventEmitter
 			return true;
 		}
 
-  		this.swarm.listen(this.port);
+		this.setOnpendingHandler = (func) => // func needs to take one args, which is msg object
+		{
+			if (typeof(func) !== 'function') { return false; }
+			this.removeAllListeners('onpending');
+			this.on('onpending', func);
+			return true;
+		}
 
+  		this.swarm.listen(this.port);
 	}
 }
 
